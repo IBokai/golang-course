9208f048da3101e227fe524d03db75e1b16f2cd2
Ivan <bokai.box@gmail.com>
2026-03-22 20:52:33 +0300

    feat: add app struct for dependency injection
    feat: move configs to internal folders

--- services/collector/cmd/main.go
+++ services/collector/cmd/main.go
@@ -1,32 +1,11 @@
 package main
 
 import (
-	collectorpb "distributed-system/api/gen"
-	"distributed-system/services/collector/config"
-	"distributed-system/services/collector/internal/adapter"
-	"distributed-system/services/collector/internal/handler"
-	"distributed-system/services/collector/internal/usecase"
-	"google.golang.org/grpc"
-	"log"
-	"net"
+	"distributed-system/services/collector/internal/app"
+	"distributed-system/services/collector/internal/config"
 )
 
 func main() {
 	cfg := config.MustLoad()
-
-	githubClient := adapter.New()
-	uc := usecase.New(githubClient)
-	grpcHandler := handler.New(uc)
-	lis, err := net.Listen("tcp", cfg.GRPCAddr)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	server := grpc.NewServer()
-	collectorpb.RegisterRepositoryServiceServer(server, grpcHandler)
-
-	log.Printf("Collector listening on %s", cfg.GRPCAddr)
-
-	if err := server.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	app.New(cfg).Run()
 }
